febac88c671c82718fbde15d184f15f647014b3e
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-11-11 19:17:31 +0530

    Create App.jsx

---
+++ Search Engine/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Searchbar from './components/Searchbar'
+import SearchResultsList from './components/SearchResultsList';
+
+function App() {
+  const[results,setResults]=useState('');
+ return (
+ <div className='App'>
+    <div className='search-bar-container'>
+    <Searchbar setResults={setResults}/>
+    <SearchResultsList results={results} />
+
+    </div>
+  </div>
+ )
+}
+
+export default App
